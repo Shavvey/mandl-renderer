c37296bf7aa0baffa4f79b610abd481928dce11a
Cole Johnson <coledjohn@gmail.com>
2024-06-27 20:58:37 -0600

    thread doesn't have stack memory access, im dumb

--- src/thread.c
+++ src/thread.c
@@ -1,20 +1,21 @@
 #include "thread.h"
-#include "window.h"
 // threads to preform the mandelbrot set update
 Worker_Thread threads[NTHREADS];
 
 void th_update() {
   int unsigned id = 0;
+  th_args args;
   for (int i = 0; i < NTHREADS; i++) {
-    threads[i].id = id;
+    threads[i].args.id = id;
+    threads[i].args.screen_buffer = &screen_buffer;
     // spawn new thread, pass the new id
     int ret = pthread_create(&threads[i].thread, (pthread_attr_t *)NULL,
                              (void *(*)(void *))th_mandl_update,
-                             (void *)&threads[i].id);
+                             (void *)&threads[i].args);
     id++;
     if (ret != 0) {
       fprintf(stderr, "Failed to create worker thread with id %d\n",
-              threads[i].id);
+              threads[i].args.id);
       // kill the thread
       pthread_exit((void *)EXIT_FAILURE);
     }
@@ -23,18 +24,46 @@ void th_update() {
 
 void th_mandl_update(void *args) {
   unsigned int th_id;
-  unsigned int *argp;
-  argp = (unsigned int *)args;
+  th_args *argp;
+  argp = (th_args *)args;
   // extract the thread id
   if (argp != NULL) {
-    th_id = *argp;
+    th_id = argp->id;
     printf("thread ID: %d\n", th_id);
   } else {
     fprintf(stderr, "Could not extract args.. Exiting program\n");
     pthread_exit((void *)EXIT_FAILURE);
   }
   // preform the actual mandelbrot set update
-  int dy = (DIM.width);
+  int dy = (DIM.height / NTHREADS) * th_id;
+  int end_y = (DIM.height / NTHREADS) * (th_id + 1) - 1;
+
+  int dx = (DIM.width / NTHREADS) * th_id;
+  int end_x = (DIM.width / NTHREADS) * (th_id + 1) - 1;
+  // extract imaginary coords of plot window
+  double istart = plot_window.i_start;
+  double iend = plot_window.i_end;
+  // real coords of plot window
+  double rstart = plot_window.r_start;
+  double rend = plot_window.r_end;
+  double normalized_color = 0;
+
+  for (; dy < end_y; dy++) {
+    for (; dx < end_x; dx++) {
+      Complex c;
+      // convert pixel coordinates into the complex plot coordinates
+      c.real = rstart + ((double)dy / DIM.height) * (rend - rstart);
+      c.img = istart + ((double)dx / DIM.width) * (iend - istart);
+      uint32_t itrs = mandl_iter(c);
+      normalized_color = lmap(itrs, 0, MAX_ITER, 0, 1);
+      uint32_t color = color_calc(normalized_color, 50, 50, 50, 1);
+      // use RGBA color calculated via the linear mapping between iterations and
+      // color strength (darker colors for converge points (ones inside the set)
+      // and lighter colors for diverging points (ones not inside the set))
+      (*argp->screen_buffer)[dy][dx] = color;
+    }
+  }
+  printf("thread ID: %d finished\n", th_id);
   // close out of thread
   pthread_exit((void *)EXIT_SUCCESS);
 }
--- src/thread.h
+++ src/thread.h
@@ -1,12 +1,18 @@
 #ifndef THREAD_H
 #define THREAD_H
 #include "mandl.h"
+#include "window.h"
 #include <pthread.h>
 #define NTHREADS 5
 
-typedef struct Worker_Thread {
+typedef struct th_args {
+  uint32_t (*screen_buffer)[WIDTH][HEIGHT];
   unsigned int id;
+} th_args;
+
+typedef struct Worker_Thread {
   pthread_t thread;
+  th_args args;
 } Worker_Thread;
 
 void th_update();
