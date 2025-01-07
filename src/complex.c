3d99d60371954375f35cd069464688394f099610
Cole Johnson <coledjohn@gmail.com>
2025-01-07 16:07:25 -0700

    comments

--- src/complex.c
+++ src/complex.c
@@ -12,6 +12,7 @@ double cmabs(Complex c) { return sqrt((c.real * c.real) + (c.img * c.img)); }
 
 // compute the square of the complex number
 Complex cmsqr(Complex c) {
+  // (a + bi)^2 = a^2 + 2abi - b^2 = a^2 - b^2 + 2abi
   Complex cmsqr = {.real = (c.real * c.real) + (c.img * c.img * -1),
                    .img = (2 * c.img * c.real)};
   return cmsqr;
